0046943367715323430b1727d873f1d2272cf889
Trushina Marina <silaevamo@gmail.com>
2024-01-06 14:49:57 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Marina Trushina");
+        System.out.println("19.02.1986");
+    }
+}
