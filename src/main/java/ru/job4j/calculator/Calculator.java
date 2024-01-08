106a14fb75c61f11a69a457c619845776a2a6556
Trushina Marina <silaevamo@gmail.com>
2024-01-08 11:16:24 +0300

    4. Методы [#231984]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,12 +1,14 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    public static void plus(int firstArgument, int secondArgument) {
+        int result = firstArgument + secondArgument;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int size = 10;
-        System.out.println(size);
-        size = 100;
-        System.out.println(size);
-        size = size - 5;
-        System.out.println(size);
+       Calculator.plus(100, 500);
+       Calculator.plus(4, 2);
+       Calculator.plus(3, 5);
     }
 }
