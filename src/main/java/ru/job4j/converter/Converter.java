d4cb7757584a42aae814bb0dcaafabd0294a6d10
Trushina Marina <silaevamo@gmail.com>
2024-01-08 17:47:01 +0300

    1. Модульные тесты. [#159435]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,21 +13,7 @@ public class Converter {
     public static void main(String[] args) {
         float euro = roubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-
         float dollarOfUsa = roubleToDollar(1000);
         System.out.println("1000 rubles are " + dollarOfUsa + " USA dollars.");
-
-        float input = 297;
-        float expected = 3;
-        float output = roubleToEuro(input);
-        boolean isPassed = expected == output;
-        System.out.println("297 rubles. It should be equal to 3. Test result is " + isPassed);
-
-        input = 450;
-        expected = 5;
-        output = roubleToDollar(input);
-        isPassed = expected == output;
-        System.out.println("450 rubles. It should be equal to 5. Test result is " + isPassed);
-
     }
 }
---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert297RubTo3Euro() {
+        float input = 297;
+        float expected = 3;
+        float output = Converter.roubleToEuro(input);
+        float value = 0.00001F;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+
+    @Test
+    void whenConvert450RubTo5Dollar() {
+        float input = 450;
+        float expected = 5;
+        float output = Converter.roubleToDollar(input);
+        float value = 0.00001F;
+        Assertions.assertThat(output).isEqualTo(expected, withPrecision(value));
+    }
+}
