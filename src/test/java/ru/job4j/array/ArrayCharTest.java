71dd518df32ce8c7019592508d57ce0d0127f11f
Trushina Marina <silaevamo@gmail.com>
2024-01-17 20:51:34 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isFalse();
+    }
+
+}
