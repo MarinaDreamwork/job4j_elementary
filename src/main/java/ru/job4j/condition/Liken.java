c0a7780f596f5223d423f5d0a70522942e6e29df
Trushina Marina <silaevamo@gmail.com>
2024-01-21 15:36:49 +0300

    0. Операции сравнения [#235631 #471884] - CI bot errors

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -5,6 +5,7 @@ public class Liken {
         int first = 10;
         int second = 9;
         boolean result = first > second;
+        System.out.println(result);
         result = first < second;
         System.out.println(result);
         result = first == second;
